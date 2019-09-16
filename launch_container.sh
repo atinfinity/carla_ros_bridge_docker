008bc14c45261b87f6526b84731481309275cfaf
atinfinity <dandelion1124@gmail.com>
2019-09-17 00:44:53 +0900

    Initial commit(Melodic)

---
+++ launch_container.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run --runtime=nvidia --privileged --rm -it \
+           --volume=$XSOCK:$XSOCK:rw \
+           --volume=$XAUTH:$XAUTH:rw \
+           --volume=$HOME:$HOME \
+           --shm-size=1gb \
+           --env="XAUTHORITY=${XAUTH}" \
+           --env="DISPLAY=${DISPLAY}" \
+           --env=TERM=xterm-256color \
+           --env=QT_X11_NO_MITSHM=1 \
+           --net=host \
+           -u "melodic"  \
+           carla:0.9.6 \
+           bash
